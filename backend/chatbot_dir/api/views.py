e8f829a1a7da739b8ca7bcb41d3cac5b2f5022d8
GlaucoInfinities <162525681+GlaucoInfinities@users.noreply.github.com>
2024-12-20 20:58:51 +1000

    logs

--- backend/chatbot_dir/api/views.py
+++ backend/chatbot_dir/api/views.py
@@ -75,13 +75,14 @@ class UserInputView(MongoDBMixin, APIView):
             # Generate response
             generation_start = time.time()
             logger.info("Starting AI response user generation")
-            #cleaned_prompt = translate_and_clean(prompt)
-            #generation = generate_user_input(cleaned_prompt)
+            
+            cleaned_prompt = translate_and_clean(prompt)
+            generation = generate_user_input(cleaned_prompt)
            # logger.info(
             #    f"AI Generation completed in {time.time() - generation_start:.2f}s"
             #)
-           # logger.info("cleaned_prompt" + cleaned_prompt)
-
+            logger.info("cleaned_prompt" + cleaned_prompt)
+            logger.info("generation" + generation)
             # Prepare MongoDB document and response data
             #response_data = {
              #   "user_id": user_id,
